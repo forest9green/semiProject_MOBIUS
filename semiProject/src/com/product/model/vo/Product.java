020632ed6236f1a9022c3aa82974fada5a423179
User <User@DESKTOP-MKIUB6S>
2021-05-17 11:52:07 +0900

    hl#4 초기 설정 완료

--- semiProject/src/com/product/model/vo/Product.java
+++ semiProject/src/com/product/model/vo/Product.java
@@ -1,5 +1,87 @@
 package com.product.model.vo;
 
+import java.util.Date;
+
 public class Product {
+	
+	private String pCode;
+	private String cateCode;
+	private int price;
+	private int stock;
+	private Date pEnrollDate;
+	private int deleveryFee;
+	private String size;
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(String pCode, String cateCode, int price, int stock, Date pEnrollDate, int deleveryFee,
+			String size) {
+		super();
+		this.pCode = pCode;
+		this.cateCode = cateCode;
+		this.price = price;
+		this.stock = stock;
+		this.pEnrollDate = pEnrollDate;
+		this.deleveryFee = deleveryFee;
+		this.size = size;
+	}
+
+	public String getpCode() {
+		return pCode;
+	}
+
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+
+	public String getCateCode() {
+		return cateCode;
+	}
+
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Date getpEnrollDate() {
+		return pEnrollDate;
+	}
+
+	public void setpEnrollDate(Date pEnrollDate) {
+		this.pEnrollDate = pEnrollDate;
+	}
+
+	public int getDeleveryFee() {
+		return deleveryFee;
+	}
+
+	public void setDeleveryFee(int deleveryFee) {
+		this.deleveryFee = deleveryFee;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
 
 }
--- semiProject/src/com/user/model/vo/User.java
+++ semiProject/src/com/user/model/vo/User.java
@@ -1,5 +1,127 @@
 package com.user.model.vo;
 
+import java.util.Date;
+
 public class User {
+	
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String email;
+	private int emailSmsCk;
+	private String cellPhone;
+	private int phoneSmsCk;
+	private String phone;
+	private Date joinDate;
+	private Date recentDate;
+	private int mileage;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String userId, String userPwd, String userName, String email, int emailSmsCk, String cellPhone,
+			int phoneSmsCk, String phone, Date joinDate, Date recentDate, int mileage) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
+		this.emailSmsCk = emailSmsCk;
+		this.cellPhone = cellPhone;
+		this.phoneSmsCk = phoneSmsCk;
+		this.phone = phone;
+		this.joinDate = joinDate;
+		this.recentDate = recentDate;
+		this.mileage = mileage;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEmailSmsCk() {
+		return emailSmsCk;
+	}
+
+	public void setEmailSmsCk(int emailSmsCk) {
+		this.emailSmsCk = emailSmsCk;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public int getPhoneSmsCk() {
+		return phoneSmsCk;
+	}
+
+	public void setPhoneSmsCk(int phoneSmsCk) {
+		this.phoneSmsCk = phoneSmsCk;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public Date getRecentDate() {
+		return recentDate;
+	}
+
+	public void setRecentDate(Date recentDate) {
+		this.recentDate = recentDate;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
 
 }
