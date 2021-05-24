d8faa46eee24e2fb2056658d2edfed360eaff439
DESKTOP-C2PDU3I\agkaw <agkawg@gmail.com>
2021-05-24 15:01:42 +0900

    ksh#7 header- NOTICE에 절대경로 설정, itemView.jsp 전부삭제, Notice(VO), NoticeDao, NoticeService,NoticeMainServlet 생성, noticeMain.jsp(작성 중)

---
+++ semiProject/src/com/notice/model/controller/NoticeMainServlet.java
@@ -0,0 +1,50 @@
+package com.notice.model.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.notice.model.service.NoticeService;
+import com.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeMainServlet
+ */
+@WebServlet("/notice/noticeMain")
+public class NoticeMainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeMainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<Notice> list = new NoticeService().selectNoticeList();
+		request.setAttribute("notice", list);
+		request.getRequestDispatcher("/views/notice/noticeMain.jsp")
+		.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
