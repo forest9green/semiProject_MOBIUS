3ce5729b8bba1fe0a8352e7f08e4e8a2230a3227
DESKTOP-C2PDU3I\agkaw <agkawg@gmail.com>
2021-05-26 15:04:13 +0900

    ksh#8 view/admin폴더에 jsp파일 추가, notice에서 noticeMain->NoiceView까지 연결성공 DB연동까지

---
+++ semiProject/src/com/notice/model/controller/NoticeViewServlet.java
@@ -0,0 +1,51 @@
+package com.notice.model.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.notice.model.service.NoticeService;
+import com.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeViewServlet
+ */
+@WebServlet("/notice/noticeView")
+public class NoticeViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String noticeNo = (request.getParameter("noticeNo"));
+		
+		Notice list = new NoticeService().selectNotice(noticeNo);
+		
+		request.setAttribute("notice", list);
+		request.getRequestDispatcher("/views/notice/noticeView.jsp")
+		.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
